adf8f643a74ee1634310ada8720dffc24e2fb83d
woon <woony8959@gmail.com>
2025-07-14 15:53:02 +0900

    "start"

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <div className="container bg-red-500 mx-auto">
+        <h1 className="text-8xl text-sky-500">Hello tailwindcssv4 and react</h1>
+        <div>
+          <h2 className="text-2xl">this is 2xl</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
